bfea239ce29a144e24759bf5adfcb51d7e7e9b90
Tianwei Cao <78243340+TianweiCao@users.noreply.github.com>
2021-01-31 00:41:09 +0800

    Update example.py

--- example.py
+++ example.py
@@ -6,16 +6,17 @@
 def add(a, b):
     return a + b
 
-
+def subtract(a, b):
+    return a - b
 
 # ============================================================
 # Defining your own testing here
 # ============================================================
 
-def test_right():
-    assert add(-2, -3) == -5
+def test_add():
+    assert add(2, 3) == 5
     assert add('boston', 'university') == 'bostonuniversity'
 
 
-def test_error():
-    assert add(2, 3) == -1
+def test_subtract():
+    assert subtract(2, 3) == -1
